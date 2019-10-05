f17fcc9bd326982ca4e076459116c552d308fde5
Aaron Lee <aaron.lee@reconstructinc.com>
2019-10-05 08:46:06 -0500

    get oauth token

---
+++ server/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const ForgeSDK = require('forge-apis');
+const app = express();
+const port = 3000;
+
+const AUTODESK_CLIENT_ID = process.env.AUTODESK_CLIENT_ID
+const AUTODESK_CLIENT_SECRET = process.env.AUTODESK_CLIENT_SECRET
+const AUTODESK_REDIRECT_URL = process.env.AUTODESK_REDIRECT_URL
+
+app.get('/', (req, res) => {res.send("Hello")})
+
+app.get('/auth/signin', (req, res) => {
+    var autoRefresh = true;
+    res.redirect(302, oauthClient().generateAuthUrl());
+})
+
+app.get('/auth/callback', (req, res) => {
+    req.query.code
+    oauthClient().getToken(req.query.code).then( (credentials) => {
+
+        const UserApi = new ForgeSDK.UserProfileApi();
+
+        UserApi.getUserProfile(oauthClient(), credentials).then( (profile) => {
+            console.log("----------")
+            console.log(profile);
+            res.send("hello again!");
+        }).catch(e => {
+            console.error(e);
+            res.send("error pulling profile");
+        });
+    }).catch(e => {
+        console.error(e);
+        res.send("login failed");
+    });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+function oauthClient() {
+    return new ForgeSDK.AuthClientThreeLegged(
+        AUTODESK_CLIENT_ID,
+        AUTODESK_CLIENT_SECRET,
+        AUTODESK_REDIRECT_URL,
+        [
+            'data:read'
+        ],
+        true);
+}
+
+function oauthToken(authCode) {
+    var credentials = ""
+    await
+    return credentials
+}
